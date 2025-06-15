fe4a0512012f6eb6dda877162963042695cf2c80
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2025-06-15 12:03:53 -0400

    build: update docker-compose

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,48 +1,53 @@
-import express from 'express';
-import type {Express} from "express"
-import helmet from 'helmet';
-import cors from 'cors';
-import rateLimit from 'express-rate-limit';
-import { env } from './env';
+import express from "express";
+import type { Express } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import { env } from "./env";
 
-import prisma from './db';
+import prisma from "./db";
 
 export function createServer(): Express {
-  const app = express();
-  app.use(express.json());
+    const app = express();
+    app.use(express.json());
 
-  app.set('trust proxy', 1);
-  app.use(helmet());
+    app.set("trust proxy", 1);
+    app.use(helmet());
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:4173',
-  env.FRONTEND_ORIGIN_1,
-  env.FRONTEND_ORIGIN_2
-];
+    const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:4173",
+        env.FRONTEND_ORIGIN_1,
+        env.FRONTEND_ORIGIN_2,
+    ];
 
-const corsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-};
+    const corsOptions = {
+        origin: (
+            origin: string | undefined,
+            callback: (err: Error | null, allow?: boolean) => void
+        ) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+        optionsSuccessStatus: 200,
+    };
 
-app.use(cors(corsOptions));
+    app.use(cors(corsOptions));
 
-  const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
-  app.use(limiter);
+    const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+    app.use(limiter);
 
-  app.get('/health', (req, res) => {res.status(200).json({message: 'OK'})});
-  app.get('/', async (req, res) => {
+    app.get("/health", (req, res) => {
+        res.status(200).json({ message: "OK" });
+    });
+    app.get("/", async (req, res) => {
+        const profiles = await prisma.user.findMany();
+        res.status(200).json({ message: "hi from the backend!", profiles });
+    });
 
-    const profiles = await prisma.user.findMany()
-    res.status(200).json({message: 'hi from the backend!',profiles})});
-
-  return app;
+    return app;
 }
