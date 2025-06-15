0e6b34c86bb725fe50dc682618948c2ae98567a2
Sidney Hopkins <sidhopkins14@gmail.com>
2025-06-15 16:46:39 -0400

    add tailwind, experiment with 3d dice roller lib

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,63 +1,41 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from 'react'
+import './App.css'
 
 // example for you for shared types!
-import type { User } from "@shared/types";
-import { Role } from "@shared/types"
+import type { User } from '@shared/types'
+import { Role } from '@shared/types'
+import DiceBoxContainer from './DiceBoxContainer'
 
 const Sidney: User = {
-    id: "some string",
-    username: "some other string",
-    password: "lamb",
-    createdAt: "today",
-    email: "something@lamb.pancake",
-    role: Role.USER
-};
-console.log(`hi ${Sidney.username}!!!`);
+    id: 'some string',
+    username: 'some other string',
+    password: 'lamb',
+    createdAt: 'today',
+    email: 'something@lamb.pancake',
+    role: Role.USER,
+}
+console.log(`hi ${Sidney.username}!!!`)
 // end of example!
 
 function App() {
-    const [count, setCount] = useState(0);
     const [info, setInfo] = useState()
 
     useEffect(() => {
         console.log(import.meta.env.VITE_BACKEND_URL)
-        fetch(import.meta.env.VITE_BACKEND_URL, {mode: "cors"}).then(
-            response => response.json()
-        ).then(data => {setInfo(data.message); console.log(data)})
-    },[])
+        fetch(import.meta.env.VITE_BACKEND_URL, { mode: 'cors' })
+            .then(response => response.json())
+            .then(data => {
+                setInfo(data.message)
+                console.log(data)
+            })
+    }, [])
 
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>Info is {info}</p>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <DiceBoxContainer />
+            <div className="text-4xl">{info}</div>
         </>
-    );
+    )
 }
 
-export default App;
+export default App
