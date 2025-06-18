1d0bcea168d196c7e9d7dfa2bc94446eb33334a0
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2025-06-17 20:03:34 -0400

    bug: fix auth route import

--- apps/backend/src/routes/auth.route.ts
+++ apps/backend/src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import passport from "passport";
-import { authFailure, authLogout } from "src/controllers/auth.controller";
+import { authFailure, authLogout } from "../controllers/auth.controller";
 
 export const authRouter: Router = Router();
 
