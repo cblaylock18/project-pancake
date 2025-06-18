4bddc10c507aeeb3ab2f4bd92c6c48d086c47c8b
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2025-06-17 20:39:12 -0400

    bug: Fix asset paths in frontend for dice roller

--- apps/frontend/src/DiceBoxContainer.tsx
+++ apps/frontend/src/DiceBoxContainer.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
 // @ts-expect-error no types for this lib, will do later
-import DiceBox from '@3d-dice/dice-box'
-
+import DiceBox from "@3d-dice/dice-box";
 
 export default function DiceBoxContainer() {
+    const diceBoxRef = useRef(null);
+    // @ts-expect-error no types for this library
+    let diceBox = null;
 
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        diceBox = new DiceBox({
+            assetPath: "/assets/", // include the trailing backslash
+            scale: 9,
+        });
 
-  const diceBoxRef = useRef(null)
-  let diceBox: any = null
+        diceBox.init().then(() => {
+            // @ts-expect-error no types for this library
+            diceBox.roll("2d20"); // Example roll
+        });
+    }, []);
 
-  useEffect(() => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      diceBox = new DiceBox("#dice-box", {
-          assetPath: './public/assets/assets', // Path to your assets
-      })
-
-      diceBox.init().then(() => {
-          diceBox.roll('2d20') // Example roll
-      })
-  }, [])
-
-  return <div ref={diceBoxRef} id="dice-box" className=""></div>
+    return <div ref={diceBoxRef} id="dice-box" className=""></div>;
 }
